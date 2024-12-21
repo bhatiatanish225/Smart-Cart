19b8fe27282255497b2a9c076b741a2633a0f2c2
ansh <ansh.bajaj2611@gmail.com>
2024-12-21 19:26:07 +0530

    third

---
+++ frontend/composeApp/src/iosMain/kotlin/org/example/smart/cart/thapar/MainViewController.kt
@@ -0,0 +1,6 @@
+package org.example.smart.cart.thapar
+
+import androidx.compose.ui.window.ComposeUIViewController
+import org.example.smart.cart.thapar.UI.MainScreen
+
+fun MainViewController() = ComposeUIViewController { MainScreen() }
